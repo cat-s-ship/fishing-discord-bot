8c7f63c46f79cb5160793fd149b54cb169cbd0e1
gretmn102 <erlgreywatson@gmail.com>
2022-12-12 06:20:32 +0200

    remove DiscordEmojisSheet.txt

--- src/Shared/Types.fs
+++ src/Shared/Types.fs
@@ -71,46 +71,6 @@ module EnabledOptionValue =
         else
             None
 
-module StandartDiscordEmoji =
-    let emojiSheetMapWidth = 42
-
-    let emojiSheetMap =
-        System.IO.File.ReadAllLines "Shared/DiscordEmojiSheet.txt"
-        |> Array.mapi (fun y ->
-            String.split " "
-            >> Seq.mapi (fun x str -> str, (x, y))
-            >> Array.ofSeq
-        )
-        |> Array.concat
-        |> Map.ofSeq
-
-
-    open SixLabors.ImageSharp
-    open SixLabors.ImageSharp.PixelFormats
-    open SixLabors.ImageSharp.Processing
-    open SixLabors.ImageSharp.Drawing.Processing
-
-    let emojiSheetUrl = "https://discord.com/assets/2071e22f8044e1f9e4f7c7afb7ac484a.png"
-    let getEmojiSheet () =
-        WebClientDownloader.getData [] emojiSheetUrl
-        |> Either.map Image.Load
-
-    let getEmoji (emojiSheet: Image<Rgba32>) (unicodeEmoji: string) (outputStream: System.IO.MemoryStream) =
-        match Map.tryFind unicodeEmoji emojiSheetMap with
-        | Some (x, y) ->
-            let emojiSize = emojiSheet.Width / emojiSheetMapWidth
-            use emoji =
-                emojiSheet.Clone(fun ctx ->
-                    let r = Rectangle(x * emojiSize, y * emojiSize, emojiSize, emojiSize)
-                    ctx.Crop r
-                    |> ignore
-                )
-            emoji.Save(outputStream, Formats.Png.PngFormat.Instance)
-
-            true
-        | None -> false
-
-
 type ResultView =
     {
         View: DSharpPlus.Entities.DiscordMessageBuilder option
